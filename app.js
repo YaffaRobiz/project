479db217756d7793b6c57fbf3cc869dbbc3c0bd6
yaffa <yaffaro@mta.ac.il>
2024-04-29 16:09:08 +0300

    main

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const express=require('express');
 const app=express();
 const adminRoutes=require('./routes/admin');
 app.use(adminRoutes);
-app.listen(3001);
+app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,8 @@ const path=require('path');
 const express=require('express');
 const router=express.Router();
 
-
+router.get('/contact',(req,res,next)=>{
+   res.sendFile(path.join(__dirname,'../','views','contact.html'))});
 router.get('/about-us',(req,res,next)=>{
     res.sendFile(path.join(__dirname,'../','views','about-us.html'))});
  router.get('/',(req,res,next)=>{
