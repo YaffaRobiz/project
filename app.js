08933c2c0679e22075c745f6a34764461a7e42a2
yaffa <yaffaro@mta.ac.il>
2024-04-29 15:20:52 +0300

    Main

---
+++ app.js
@@ -0,0 +1,5 @@
+const express=require('express');
+const app=express();
+const adminRoutes=require('./routes/admin');
+app.use(adminRoutes);
+app.listen(3001);
---
+++ routes/admin.js
@@ -0,0 +1,12 @@
+const path=require('path');
+const express=require('express');
+const router=express.Router();
+
+
+router.get('/about-us',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'../','views','about-us.html'))});
+ router.get('/',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'../','views','index.html'))});
+ router.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'../','views','404.html'))});
+ module.exports=router;
