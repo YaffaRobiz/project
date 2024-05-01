b43e3bc32b73e1493c578a8e354f2fa2fafe7ecd
yaffa <yaffaro@mta.ac.il>
2024-05-01 18:42:34 +0300

    Added style.css, no more style in header

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const path=require('path');
 const express=require('express');
 const app=express();
 const adminRoutes=require('./routes/admin');
+app.use(express.static(path.join(__dirname,'public')));
 app.use(adminRoutes);
-app.listen(3000);
+app.listen(3002);
